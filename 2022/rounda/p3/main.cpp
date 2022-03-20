0efc28f26f7801ad215192c2f44d41c5050a3cb0
Subha Das <rohan6056@hotmail.com>
2022-03-20 00:27:25 -0700

    initial commit

---
+++ 2022/rounda/p3/main.cpp
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cerr<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ 
+
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0);
+    if (sz(name)) {
+        (void)(freopen((name + ".in").c_str(), "r", stdin)+1);
+        (void)(freopen((name + ".out").c_str(), "w", stdout)+1);
+    }
+}
+void solve(){
+    ll n;
+    string s;
+    cin >> n;
+    cin >> s;
+    
+}
+int main()
+{
+    setIO();
+    ll t;
+    cin >> t;
+    for(int it=1;it<=t;it++) {
+     cout << "Case #" << it << ": ";
+        solve();
+        if (it<t) cout << ln;
+    }
+    return 0;
+}
